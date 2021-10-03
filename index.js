b3493d8e181d9fad82f91140aff8e2c3639b9471
Aspect <68440211+Aspectious@users.noreply.github.com>
2021-10-03 11:27:52 -0400

    dcgxn,nm,hlhjkvjyuvjtyv

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
+const colors = require ("colors")
 
 const TokenList = require("../token.json"); // Important for hosting.
-const config = require("./configuration.json");
-const MessageHandler = require("./message");
+const config = require("./configuration.json")
 
 class GME {
     constructor() {
@@ -15,13 +15,20 @@ class GME {
         
             const prefix = config.global_prefix;
 
+        // Events
+
+        client.on("debug", message => {
+            console.debug(message)
+        });
+
         client.on("message", message => {
-        })
+        });
 
         client.login(TokenList.GME_TOKEN).then(() => {
         this.load();
         });
-}
+}ggfgggycfyjfgvvgjhgvhcfhcchgghcfhgvgnvgvgv
+
 load() {
     this.client.user.setActivity(config.status, {type:"COMPETING"})
 }
--- message.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = function MessageHandler(rawmessage) {
-const message = Discord.Message(rawmessage)
-if (message.content)
-}

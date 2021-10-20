6aea8ecc4111d9e43285ad4e775562fa6bfa5e3a
Aspect <68440211+Aspectious@users.noreply.github.com>
2021-10-20 13:11:56 -0400

    Update gameEnviroment.js

--- components/gameEnviroment.js
+++ components/gameEnviroment.js
@@ -1,5 +1,15 @@
 const Discord = require("discord.js")
 
 module.exports = class gameEnviroment {
-constructor(hostID,
+constructor(hostID,participants,gameID,debugMode) {
+  this.hostID = hostID;
+  this.participants = participants;
+  this.gameID = gameID;
+  this.debugMode = debugMode;
+  
+  if ((debugMode != false)||(debugMode)) console.debug(`DEBUG\ GME GameEnviroment Server started in Debug Mode, running with Parameters HOST={${this.hostID}}, PARTICIPANTS={${this.participants}}, GAMEID={${this.gameID}}`);
+  this.serverStart();
+}
+  serverStart() {
+  }
 }
